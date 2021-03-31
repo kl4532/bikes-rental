ae5428d6b27057244a048b2aa081f442dcd5ba6f
kl4532 <kornellukasiak@gmail.com>
2021-03-31 13:00:54 +0200

    add base components for generic view

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,16 @@
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home.component';
+import { HomeRoutingModule } from './home-routing.module';
+
+@NgModule({
+  imports: [
+    HomeRoutingModule
+  ],
+  declarations: [
+    HomeComponent
+  ],
+  providers: []
+})
+export class HomeModule {}
---
+++ src/app/shared/layout/layout-header/layout-header.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-layout-header',
+  templateUrl: './layout-header.component.html',
+  styleUrls: ['./layout-header.component.scss']
+})
+export class LayoutHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
