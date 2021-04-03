8db11a2ee5e927effe9dd2b60e7e7ffe9b50fde9
kl4532 <kornellukasiak@gmail.com>
2021-04-03 23:42:51 +0200

    add min max date to date picker, add max price

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,18 +12,34 @@ export class HomeComponent implements OnInit {
   searchForm: any;
   formSubmited = false;
 
+  minDate = new Date();
+  maxDate = new Date();
+
   constructor() { }
 
   ngOnInit(): void {
+    const maxPeriod = 60;
+    this.minDate = new Date();
+    this.maxDate = new Date(new Date().getTime() + (maxPeriod * 24 * 60 * 60 * 1000));
+
+
     this.searchForm = new FormGroup({
       bikeTypes: new FormControl('', Validators.required),
       dateStart: new FormControl('', Validators.required),
-      dateEnd: new FormControl('', Validators.required)
+      dateEnd: new FormControl('', Validators.required),
+      maxPrice: new FormControl('')
     });
   }
+
   onSubmit() {
     console.warn(this.searchForm);
     this.formSubmited = true;
   }
+
+  calculateRange() {
+    // TOFIX set date range dynamically from selected min + 14 days
+    // this.maxDate = new Date(selected + (14 * 24 * 60 * 60 * 1000));
+  }
+
 
 }
