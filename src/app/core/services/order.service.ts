2f8fd7dfc28a00e37c1a029fe8230a005c43ae9a
kl4532 <kornellukasiak@gmail.com>
2021-06-11 21:42:30 +0200

    start with order and user crud

--- src/app/core/services/order.service.ts
+++ src/app/core/services/order.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@angular/core';
+import {Inject, Injectable} from '@angular/core';
 import {Item} from '../models/item.model';
 import {BehaviorSubject, Observable, of, Subject} from 'rxjs';
 import {DomSanitizer} from "@angular/platform-browser";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +12,13 @@ export class OrderService {
   private order: Item[] = [];
   totalPrice: any;
   orderChange$: BehaviorSubject<Item[]> = new BehaviorSubject<Item[]>(this.order);
+  url = `${this.baseUrl}orders`;
 
   private ls = window.localStorage;
 
-  constructor(private sanitizer: DomSanitizer) { }
+  constructor(private sanitizer: DomSanitizer,
+              private http: HttpClient,
+              @Inject('API_URL') private baseUrl: string) { }
 
   addToOrder(item: Item): void {
     this.getOrderFromLocalStorage();
@@ -47,9 +51,31 @@ export class OrderService {
     console.log('Order submitted');
     console.log('User details', userDetails);
     console.log('Order details', items);
-    const order = {items, user: userDetails};
+    const order = {bookedDates: this.createBookedDates(items), user: userDetails};
+    console.log(order);
     // http post request with reservation
+    this.http.post(this.url, order).subscribe(
+      data => console.log(data),
+      err => console.log(err),
+      () => console.log('Sent successfully order:', order)
+    );
     return of(null);
+  }
+
+  createBookedDates(items: Item[]): any{
+    const formatted = [];
+    for (const item of items) {
+      const bookedDate = {
+        bikeId: item.bike.id,
+        dateStart: item.dateStart,
+        dateEnd: item.dateEnd,
+        bike: item.bike
+      };
+      formatted.push(bookedDate);
+    }
+    console.log('formatted', formatted);
+
+    return formatted;
   }
 
   getTotalPrice(items: Item[]): number {
