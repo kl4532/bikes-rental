6f57fb4ef6a0ccdbcf5b48edcd25dc22ae4e7178
kl4532 <kornellukasiak@gmail.com>
2021-06-27 08:29:03 +0200

    refactor, add unsubs

--- src/app/admin/bike-editor/bike-editor.component.ts
+++ src/app/admin/bike-editor/bike-editor.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {BikesService} from "../../core/services/bikes.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {Bike} from "../../core/models/bike.model";
 import { MaxSizeValidator } from '@angular-material-components/file-input';
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-bike-editor',
   templateUrl: './bike-editor.component.html',
   styleUrls: ['./bike-editor.component.scss']
 })
-export class BikeEditorComponent implements OnInit {
+export class BikeEditorComponent implements OnInit, OnDestroy {
 
   bikeId = 0;
   bikeForm: FormGroup = new FormGroup({});
@@ -18,6 +19,7 @@ export class BikeEditorComponent implements OnInit {
   modeEdit = false;
   bike: any;
   maxImgSizeMb = 3;
+  subscriptions = new Subscription();
 
   constructor(private activatedRoute: ActivatedRoute,
               private bikeService: BikesService,
@@ -25,52 +27,46 @@ export class BikeEditorComponent implements OnInit {
               private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-    this.init();
-
-    this.activatedRoute.params.subscribe(params => {
+    const arSub = this.activatedRoute.params.subscribe(params => {
       const param = 'id';
       this.bikeId = +params[param];
+
+      this.bikeForm = new FormGroup({
+        picture: new FormControl(null,[
+          MaxSizeValidator(this.maxImgSizeMb * 1048576)
+        ] ),
+        name: new FormControl('Test',[
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(20)]),
+        description: new FormControl('test',[
+          Validators.required]),
+        price: new FormControl('10',[
+          Validators.required]),
+        type: new FormControl('road',[
+          Validators.required]),
+        size: new FormControl('M',[
+          Validators.required]),
+        status: new FormControl('available',[
+          Validators.required]),
+        gear: this.formBuilder.array([])
+      });
+
       this.modeEdit = !!this.bikeId;
       if (this.modeEdit) {
-        this.bikeService.getBikeDetails(this.bikeId).subscribe((bike: Bike) => {
+        const bdSub = this.bikeService.getBikeDetails(this.bikeId).subscribe((bike: Bike) => {
           this.bike = bike;
           this.setForm();
         });
+        this.subscriptions.add(bdSub);
       }
 
     });
-  }
-
-  init(): void {
-    this.activatedRoute.params.subscribe(params => {
-      const param = 'id';
-      this.bikeId = params[param];
-    });
-
-    this.bikeForm = new FormGroup({
-      picture: new FormControl(null,[
-        MaxSizeValidator(this.maxImgSizeMb * 1048576)
-      ] ),
-      name: new FormControl('Test',[
-        Validators.required,
-        Validators.minLength(3),
-        Validators.maxLength(20)]),
-      description: new FormControl('test',[
-        Validators.required]),
-      price: new FormControl('10',[
-        Validators.required]),
-      type: new FormControl('road',[
-        Validators.required]),
-      size: new FormControl('M',[
-        Validators.required]),
-      status: new FormControl('available',[
-        Validators.required]),
-      gear: this.formBuilder.array([])
-    });
+    this.subscriptions.add(arSub);
   }
 
   setForm(): void {
-    this.bikeService.getBikeDetails(this.bikeId).subscribe((bike: Bike) => {
+    const bdSub = this.bikeService.getBikeDetails(this.bikeId).subscribe((bike: Bike) => {
       this.bikeForm.setValue({
         picture: bike.picture,
         name: bike.name,
@@ -88,8 +84,8 @@ export class BikeEditorComponent implements OnInit {
         }
       }
     });
+    this.subscriptions.add(bdSub);
   }
-
 
   createItem(name: string): FormGroup {
     return this.formBuilder.group({
@@ -122,6 +118,10 @@ export class BikeEditorComponent implements OnInit {
 
   generateUUID(): number {
     return new Date().getTime() + Math.round(Math.random() * 1E7);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 
 }
